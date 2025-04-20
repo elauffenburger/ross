c22c5811f3c7eca083d2ad136f4bcc335bd688fa
Eric Lauffenburger <elauffenburger@gmail.com>
2025-04-20 00:28:51 -0700

    wip:

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,5 +1,19 @@
+; tty mode
+mov ah, 0x0e
+
+; print Hello
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
 loop:
   jmp loop
 
-times 510-($-$$) db 0
+times 510 - ($-$$) db 0
 dw 0xaa55
---
+++ notes/bios.md
@@ -0,0 +1,4 @@
+## References
+
+- [BIOS Interrupts Overview](https://en.wikipedia.org/wiki/BIOS_interrupt_call#Interrupt_table)
+- [BIOS Interrups List](https://www.cs.cmu.edu/~ralf/files.html)
