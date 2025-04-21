e20d3c917084dcb096e81b3c5a9c07ae2b2a875a
Eric Lauffenburger <elauffenburger@gmail.com>
2025-04-20 20:25:44 -0700

    wip:

--- build.zig
+++ build.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const fs = std.fs;
 
 pub fn build(b: *std.Build) !void {
-    const exe = addExe(b);
-    addBuildIso(b, exe);
-
+    const exe = addInstall(b);
     addTests(b, exe.root_module);
+
+    addBuildIso(b);
 }
 
-fn addExe(b: *std.Build) *std.Build.Step.Compile {
+fn addInstall(b: *std.Build) *std.Build.Step.Compile {
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = b.standardTargetOptions(.{
@@ -46,23 +46,18 @@ fn addTests(b: *std.Build, exe_mod: *std.Build.Module) void {
     test_step.dependOn(&run_exe_unit_tests.step);
 }
 
-fn addBuildIso(b: *std.Build, exe: *std.Build.Step.Compile) void {
+fn addBuildIso(b: *std.Build) void {
     const step = b.step("build-iso", "Build iso");
-    const copy_files = b.addWriteFiles();
-    const mkisofs = std.Build.Step.Run.create(b, "mkisofs");
-
-    b.getInstallStep().dependOn(step);
-    step.dependOn(&mkisofs.step);
-    mkisofs.step.dependOn(&copy_files.step);
-    copy_files.step.dependOn(&exe.step);
 
     // Copy files to the iso staging dir.
+    const copy_files = b.addWriteFiles();
     _ = copy_files.addCopyFile(b.path("boot/stage2_eltorito"), "out/iso/boot/grub/stage2_eltorito");
     _ = copy_files.addCopyFile(b.path("boot/menu.lst"), "out/iso/boot/grub/menu.lst");
     const kernel_elf_file = copy_files.addCopyFile(b.path("zig-out/bin/ross"), "out/iso/boot/kernel.elf");
     const iso_dir = kernel_elf_file.path(b, "../../");
 
     // Run mkisofs on iso staging dir.
+    const mkisofs = std.Build.Step.Run.create(b, "mkisofs");
     mkisofs.addArgs(&.{
         "mkisofs",
         "-quiet",
@@ -78,11 +73,16 @@ fn addBuildIso(b: *std.Build, exe: *std.Build.Step.Compile) void {
         "-A",
         "os",
     });
-
     mkisofs.addArg("-o");
     const iso_file = mkisofs.addOutputFileArg(b.path("out/os.iso").getPath(b));
-
     mkisofs.addDirectoryArg(iso_dir);
 
-    b.getInstallStep().dependOn(&b.addInstallFile(iso_file, "os.iso").step);
+    // Install os.iso
+    const install_iso_file = b.addInstallFile(iso_file, "os.iso");
+
+    // install -> copy_files -> mkiso -> copy_iso -> build_iso
+    copy_files.step.dependOn(b.getInstallStep());
+    mkisofs.step.dependOn(&copy_files.step);
+    install_iso_file.step.dependOn(&mkisofs.step);
+    step.dependOn(&install_iso_file.step);
 }
